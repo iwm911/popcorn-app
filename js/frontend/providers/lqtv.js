a9d9e1cb32630b3535694efa89201ce5cd3903ea
K M <k3d3@github.com>
2014-03-17 00:44:24 -0400

    LqTv model is probably good enough for getting popular TV now - the views can be adapted aound it

--- js/frontend/providers/lqtv.js
+++ js/frontend/providers/lqtv.js
@@ -1,7 +1,20 @@
 var lowqualitytv = require('./js/frontend/providers/lowqualitytv');
 
 var LqTv = Backbone.Collection.extend({
-  model: App.Model.Tv
+  model: App.Model.Tv,
+
+  initialize: function(models, options) {
+    this.options = options;
+    LqTv.__super__.initialize.apply(this, arguments);
+  },
+
+  fetch: function() {
+    lowqualitytv.getPopularTelevisionDetails(function(tvData) {
+      collection.set(tvData);
+      collection.trigger('loaded');
+      console.log(tvData);
+    });
+  }
 });
 
 App.Providers.LqTv = LqTv;
