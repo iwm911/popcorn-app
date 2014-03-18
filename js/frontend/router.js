65a9ce0be3dab348371796a843df5e6601796df5
jcarrharris <joncarrharris@gmail.com>
2014-03-17 23:45:42 -0400

    modifying routes

--- js/frontend/router.js
+++ js/frontend/router.js
@@ -1,8 +1,11 @@
 var Router = Backbone.Router.extend({
     routes: {
-        'index(:page).html':   App.Controller.Home,
+        'index(:page).html': App.Controller.Home,
+        'index2(:page).html': App.Controller.Hometv,
         'search/:term(/:page)': App.Controller.Search,
-        'filter/:genre(/:page)': App.Controller.FilterGenre
+        'searchtv/:term(/:page)': App.Controller.Searchtv,
+        'filter/:genre(/:page)': App.Controller.FilterGenre,
+        'filtertv/:genre(/:page)': App.Controller.FiltertvGenre
     }
 });
 
